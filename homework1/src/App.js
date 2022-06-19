21bd9fb27f5ca293335b1c5129b0fc19a22c6b28
MewPk <milzaza123@gmail.com>
2022-06-19 14:39:17 +0700

    finish

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./Components/AboutUs";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
 export default function App() {
   return (
     <Router>
       <Routes>
-
-        <Route path='/'></Route>
-
-
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/portfolio" element={<Portfolio/>}></Route>
+        <Route path="/aboutus" element={<AboutUs/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
       </Routes>
     </Router>
-    
-  )
+  );
 }
