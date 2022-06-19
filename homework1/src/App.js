566cf28296c71eb7499aefa1c431a77c7b930055
MewPk <milzaza123@gmail.com>
2022-06-19 14:08:55 +0700

    Update

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import Home from './Components/Home'
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+          <Route path='/'  element={<Home/>}></Route>
+
+      </Routes>
+    </Router>
   )
 }
