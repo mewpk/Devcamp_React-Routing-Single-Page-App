910e268eca19d25e8c165eb9ba0e7e31c58583ae
MewPk <milzaza123@gmail.com>
2022-06-19 14:33:01 +0700

    create new react

--- homework1/src/App.js
+++ homework1/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
-import Home from './Components/Home'
 export default function App() {
   return (
     <Router>
       <Routes>
-          <Route path='/'  element={<Home/>}></Route>
+
+        <Route path='/'></Route>
+
 
       </Routes>
     </Router>
+    
   )
 }
